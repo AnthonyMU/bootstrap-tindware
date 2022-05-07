222213fcdd5c549b1032120496c28e34944e39f2
9c23a5 <40715125+9c23a5@users.noreply.github.com>
2022-05-07 21:25:55 +0200

    no se si esto va a funcionar yo lo pongo y a ver lo que sale

---
+++ do_login.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<head>
+    <title>Iniciar sesión - Tindware</title>
+    <link rel="stylesheet" type="text/css" href="css/do_login.css">
+</head>
+<body>
+    <?php
+    include 'inc/constantes.php';
+    @include "inc/obtenerDatosUser.php";
+    include 'inc/header.php';
+    # Recogemos datos del POST
+    @$login = $_POST['login'];
+    @$passwd = $_POST['passwd'];
+    $con = mysqli_connect('localhost', 'root', MYSQL_PASSWD);
+    if (str_contains($login, '@')) {
+        # Ha iniciado sesión utilizando un correo
+        $query = "SELECT id, username FROM tindware.usuario WHERE email = '$login' AND passwd = '$passwd' LIMIT 1";
+    }
+    else {
+        # Ha iniciado sesión utilizando un username
+        $query = "SELECT id, username FROM tindware.usuario WHERE username = '$login' AND passwd = '$passwd' LIMIT 1";
+    }
+    
+    $out = mysqli_query($con, $query);
+    if (mysqli_num_rows($out) == 1) {
+        #echo "Datos correctos"; # DEBUG
+        $loginCorrecto = true;
+        $resultado = mysqli_fetch_array($out);
+
+        # Y obtenemos los datos del usuario mediante su id
+        $id = $resultado['id'];
+        $username = $resultado['username'];
+        $datosUser = obtenerDatosUser($id);
+
+        # Setteamos las cookies
+        setcookie('id_user', $id, 0, '/');
+        setcookie('type_user', $datosUser['tipo'], 0, '/');
+        $query = "UPDATE tindware.usuario SET lastlogin = current_timestamp() WHERE id = $id;";
+        mysqli_query($con, $query);
+    }
+    else {
+        $loginCorrecto = false;
+        echo "<a href='iniciosesion.php'>Datos incorrectos, pincha aquí para rellenar los datos de inicio de sesión correctamente</a>"; # DEBUG
+    }
+    ?>
+
+    <?php
+    # Si el usuario no ha introducido los datos correctos escondemos el div de success
+    if (!$loginCorrecto) {
+         echo HIDE;
+     }
+    ?>
+    <div id="main">
+        <?php
+        # Si el usuario no ha introducido los datos correctos escondemos el div de success
+        if (!$loginCorrecto) {
+            echo HIDE;
+        }
+        ?>
+        <div id="dologin_success">
+
+        <p class="bienvenido">Bienvenido de nuevo, <?php echo $username ?>. 
+        <p><a href="index.php" class="volver">Volver al inicio</a></p>
+        </div>
+        <?php
+        # Si el usuario no ha introducido los datos correctos escondemos el div de success
+        if (!$loginCorrecto) {
+            echo HIDECLOSE;
+        }
+        ?>
+
+        <?php
+        # Si el inicio de sesión es correcto escondemos el div de error
+        if ($loginCorrecto) {
+            echo HIDE;
+        }
+        ?>
+        <div id="dologin_error">
+            <center class="bienvenido">El usuaio o la contraseña son inválidos. <a href="login.php" class="volver">Vuelve a intentarlo</a> o <a href="index.php" class="volver">vuelve al inicio</a></center>
+            <!-- Error cuando ya has iniciado sesión -->
+        </div>
+        <?php
+        # Si el inicio de sesión es correcto escondemos el div de error
+        if ($loginCorrecto) {
+            echo HIDECLOSE;
+        }
+        mysqli_close($con);
+        ?>
+    </div>
+</body>
+</html>
--- iniciosesion.php
+++ iniciosesion.php
@@ -11,33 +11,56 @@
     # Incluir header
     include 'inc/header.php';
 ?> 
-
-   <div class="container">
+<?php
+    if ($id_user != 0) {
+        echo HIDE;
+    }
+?>
+<div class="container">
     <div class="row justify-content-center pt-5 mt-5">
         <div class="col-md-6 col-sm-8 col-xl-6 col-lg-5 formulario">
-            <form action="">
-            <div class="form-group text-center">
-                 <h4 class="text-white">Iniciar sesion</h4>
-                 <div id="centrador">
-                    <img src="img/login2.png" class="rounded-circle img">  
+            <form action="do_login.php" method="POST">
+                <div class="form-group text-center">
+                    <h4 class="text-white">Iniciar sesion</h4>
+                    <div id="centrador">
+                        <img src="img/login2.png" class="rounded-circle img">  
+                    </div>
                 </div>
-            </div>
-            <div class="form-group mx-sm-4 pb-3">
-                <input type="text" class="form-control" placeholder="Usuario">
-            </div>
-            <div class="form-group mx-sm-4 pb-3">
-                <input type="password" class="form-control" placeholder="Contraseña">
-            </div>
-            <div class="form-group mx-sm-4 pb-4">
-                <input type="submit" class="btn btn-block ingresar" value="ingresar">
-            </div>
-            <div class="form-group mx-sm-4 pb-3 text-center">
-                <a href="registro.html" class="text-warning">¿No tienes una cuenta?</a>
-            </div>
+                <div class="form-group mx-sm-4 pb-3">
+                    <input name="login" type="text" class="form-control" placeholder="Usuario o email">
+                </div>
+                <div class="form-group mx-sm-4 pb-3">
+                    <input name="passwd" type="password" class="form-control" placeholder="Contraseña">
+                </div>
+                <div class="form-group mx-sm-4 pb-4">
+                    <input type="submit" class="btn btn-block ingresar" value="Iniciar sesión">
+                </div>
+                <div class="form-group mx-sm-4 pb-3 text-center">
+                    <a href="registro.html" class="text-warning">¿No tienes una cuenta?</a>
+                </div>
             </form>  
         </div>   
     </div>
 </div>
+<?php
+if ($id_user != 0) {
+    echo HIDECLOSE;
+}
+if ($id_user == 0) {
+    echo HIDE;
+}
+?>
+<div class="container">
+    <div class="row justify-content-center pt-5 mt-5">
+        <p>Ya has iniciado sesión!</p>
+        <a href="index.php" class="btn btn-block ingresar">Volver al inicio</a>
+    </div>
+</div>
+<?php
+if ($id_user == 0) {
+    echo HIDECLOSE;
+}
+?>
 
 <!-- Footer -->
 
