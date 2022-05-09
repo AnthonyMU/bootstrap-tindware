50d1b7551c54b4f3147123c27bf95de581310e9c
AnthonyMU <anthonyrodriguezmurriel@gmail.com>
2022-05-09 10:06:40 +0200

    alinear

--- FAQ.php
+++ FAQ.php
@@ -19,7 +19,7 @@
 <div class="accordion accordion-flush py-4 p-5" id="accordionFlushExample">
     <h2 class="text-center text-white py-4 ">Preguntas más frecuentes</h2>
     <div class="accordion-item faq text-white text-center">
-      <h2 class="accordion-header faq" id="flush-headingOne">
+      <h2 class="accordion-header faq text-center" id="flush-headingOne">
         <button class="accordion-button collapsed faq alinear" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne faq">
           ¿Puedo elegir al técnico que va a solventar mi problema?
         </button>
