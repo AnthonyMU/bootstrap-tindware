606cb942e88a09bd7a7069ac319d796721c8b116
AnthonyMU <anthonyrodriguezmurriel@gmail.com>
2022-05-17 16:12:16 +0200

    prueba

--- registro.php
+++ registro.php
@@ -53,7 +53,7 @@
                     <div class="form-group mx-sm-4 pb-3">
                         <input type="text" class="form-control" id="geocoding_input" placeholder="Escriba su dirección..."/> 
                         <button type="button" class="cc" onclick="coordenadasDesdeGeocoding();">Asignar coordenadas</button><br/>
-                        <p> Latitud: <input type="text" size="5" onkeydown="return false;" style="pointer-events: none;" id="lat" name="lat" required value=""/><p></br>
+                        <p> Latitud: <input type="text" size="5" onkeydown="return false;" style="pointer-events: none;" id="lat" name="lat" required value=""/><p>
                         <p>Longitud: <input type="text" size="5" onkeydown="return false;" style="pointer-events: none;" name="lon" id="lon" required value=""/></p>
                     <div class="form-group mx-sm-4 pb-3">
                         <input type="submit" class="btn btn-block ingresar" value="Registrarse">
