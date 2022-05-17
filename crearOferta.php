5cd16a2af9d6e1160f9d431c01afe28d09731cf9
AnthonyMU <anthonyrodriguezmurriel@gmail.com>
2022-05-17 12:09:23 +0200

    ajustando crear oferta

--- crearOferta.php
+++ crearOferta.php
@@ -14,14 +14,15 @@
     ?>
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-auto text-center">
+            <div class="col-auto text-center w-100">
                <form action="do_crearoferta.php" method="POST">
                     <!-- Formulario creación oferta -->
                 <h3>Titulo</h3>
-                    <input type="text" name="titulo" required/>
+                    <input type="text" class="w-50" name="titulo" required/>
                 <h3>Descripción</h3>
-                    <textarea type="text" name="descripcion" required cols="50" rows="10" placeholder="Rellena este campo"></textarea>  <!-- Cols y rows para que se vea bien en el navegador -->
-                    <input type="submit" value="Crear"/>   
+                    <textarea type="text" name="descripcion"  class="w-50" placeholder="Rellena este campo"></textarea>
+                     
+                    <input type="submit" class="btn btn-block ingresar" value="Crear oferta"> 
             </form>  
             </div>
         </div>
