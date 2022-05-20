38e4445013745fb3ca918169c8d4ad5eef5cf03a
AnthonyMU <anthonyrodriguezmurriel@gmail.com>
2022-05-20 09:38:26 +0200

    probando el btn

--- ofertas_part.php
+++ ofertas_part.php
@@ -79,6 +79,7 @@
                     echo "<p class='card-text'>$descripcion</p><br/>";
                     if ($tipo = 'disponible') {
                         echo "<span class='iroferta'><a href='viewOferta.php?id=$id_oferta'>Ver más</a></span><br/>";
+                        echo "<input type='button' class='binput' onclick='location.href='viewOferta.php?id=$id_oferta'>Ver más</input>'";
                     }
                     #echo "<form method='post' action='do_asignar_terminar.php'><input type='submit' value='";
                     #if (!$clickable) {echo " disbaled";}
