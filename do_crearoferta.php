c2a3f37035ae7edc989f4039b9a9d2d5f87cfec1
AnthonyMU <anthonyrodriguezmurriel@gmail.com>
2022-05-21 10:56:11 +0200

    text-white

--- do_crearoferta.php
+++ do_crearoferta.php
@@ -83,7 +83,7 @@
         <div id="docrearoferta_success" class="container lead">
         <div class="row justify-content-center mt-4 mb-4">
             <div class="col-auto">
-                <p>Se ha creado su oferta satisfactoriamente, ¡pronto un técnico se pondrá en contacto con usted!</p>
+                <p class="text-white">Se ha creado su oferta satisfactoriamente, ¡pronto un técnico se pondrá en contacto con usted!</p>
                     <p class="text-center">¿Qué deseas hacer a continuación?</p>
                  <input type="button" class="binput mt-5" onclick="location.href='crearOferta.php'" value="Nueva">
                 <?php echo "<input type='button' class='binput' onclick=location.href='viewOferta.php?id=$id_oferta' value='Más información'>";?>
